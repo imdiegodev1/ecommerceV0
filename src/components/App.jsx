9f11a1bf99f9c0d2785569f5e8be4f225f06a1e0
imdiegodev1 <diegoanr40@hotmail.com>
2023-03-02 20:18:26 -0500

    project setup

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/global.scss';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hola Mundo</h1>
+            <p>first steps seting from 0 a react project</p>    
+        </div>
+    )
+}
+
+export default App;
